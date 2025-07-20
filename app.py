662f903707a023f5a3a9bf74f339f6c46e76dfc3
Akila27-18 <akila271819@gmail.com>
2025-07-21 00:09:46 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+import os
+from werkzeug.utils import secure_filename
+
+app = Flask(__name__)
+UPLOAD_FOLDER = 'static/uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file.filename != '':
+            filename = secure_filename(file.filename)
+            path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(path)
+            return redirect(url_for('gallery'))
+    return render_template('upload.html')
+
+@app.route('/gallery')
+def gallery():
+    files = os.listdir(UPLOAD_FOLDER)
+    return render_template('gallery.html', files=files)
+
+@app.route('/file/<filename>')
+def file_view(filename):
+    return render_template('file.html', filename=filename)
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ static/js/script.js
@@ -0,0 +1,1 @@
+console.log("File Upload Gallery loaded!");
